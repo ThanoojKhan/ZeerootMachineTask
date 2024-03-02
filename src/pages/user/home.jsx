5ba67ab522b8a5f3cdbce36644c4ba89dab8421d
Thanooj <thanu1495@gmail.com>
2024-03-02 22:02:27 +0530

    images changed

--- src/pages/user/home.jsx
+++ src/pages/user/home.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import Navbar from '../../components/navbar'
-import Blocks from '../../components/homePage/blocks'
-import Features from '../../components/homePage/features'
-import Hero from '../../components/homePage/hero'
-import Newsletter from '../../components/homePage/newsletter'
-import Testimonials from '../../components/homePage/testimonials'
-import Goals from '../../components/homePage/goals'
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../components/navbar';
+import Blocks from '../../components/homePage/blocks';
+import Features from '../../components/homePage/features';
+import Hero from '../../components/homePage/hero';
+import Newsletter from '../../components/homePage/newsletter';
+import Testimonials from '../../components/homePage/testimonials';
+import Goals from '../../components/homePage/goals';
+import Loader from '../../components/loader';
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Blocks />
-      <Testimonials />
-      <Goals />
-      <Features />
-      <Newsletter />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <Blocks />
+          <Testimonials />
+          <Goals />
+          <Features />
+          <Newsletter />
+        </>
+      )}
     </>
-  )
+  );
 }
 
 export default Home;
